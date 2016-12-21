76cb425a8cc18aa8477e278ecbfe4c0e9829ff3b
sx-9145 <sx-gongtc@dtdream.com>
2016-12-21 16:48:37 +0800

    Initial commit

---
+++ src/main/java/com.gtc/controller.java
@@ -0,0 +1,14 @@
+package com.gtc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by gtc on 2016/12/20.
+ */
+@Controller
+public class controller {
+
+    @Autowired
+
+}
